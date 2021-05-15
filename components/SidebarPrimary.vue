3980e92beae0e06f26499c5223744da9999d4c20
thekhairul <thekhairul.office@gmail.com>
2021-05-15 13:20:35 +0600

    polish primary sidebar and add tooltip

--- components/SidebarPrimary.vue
+++ components/SidebarPrimary.vue
@@ -1,17 +1,26 @@
 <template>
-  <aside id="menubar" class="border h-full w-16 flex flex-col justify-between">
+  <aside
+    id="menubar"
+    class="shadow-lg h-full w-16 flex flex-col justify-between"
+  >
     <button class="w-16 h-16 bg-blue-500 hover:bg-blue-600">
       <fa :icon="['fas', 'bars']" class="text-white text-2xl" />
     </button>
 
     <nav>
       <ul>
-        <li v-for="(item, idx) in navmenus" :key="idx" class="text-center p-3">
-          <NuxtLink :to="item.route"
-            ><fa
-              :icon="['fas', item.icon]"
-              class="text-blue-500 text-2xl hover:text-blue-600"
-          /></NuxtLink>
+        <li
+          v-for="(item, idx) in navmenus"
+          :key="idx"
+          class="text-center px-3 py-4"
+        >
+          <NuxtLink
+            :to="item.route"
+            class="flex justify-center text-2xl text-gray-500 hover:text-blue-600"
+            :data-tooltip="item.title"
+            exact-active-class="nuxt-link-exact-active text-blue-600"
+            ><component :is="item.icon" class="fill-current"></component
+          ></NuxtLink>
         </li>
       </ul>
     </nav>
@@ -26,30 +35,42 @@
 </template>
 
 <script>
+import HomeIcon from '~/assets/images/home.svg?inline';
+import WorkIcon from '~/assets/images/briefcase.svg?inline';
+import CertificateIcon from '~/assets/images/graduation-hat.svg?inline';
+import BlogIcon from '~/assets/images/pencil.svg?inline';
+import ContactIcon from '~/assets/images/phone.svg?inline';
+
 export default {
   name: 'SidebarPrimary',
+  components: { HomeIcon, WorkIcon, CertificateIcon, BlogIcon, ContactIcon },
   data() {
     return {
       navmenus: [
         {
           route: '/',
-          icon: 'user-circle',
+          icon: 'home-icon',
+          title: 'Profile',
         },
         {
           route: '/work',
-          icon: 'briefcase',
+          icon: 'work-icon',
+          title: 'Experience',
         },
         {
           route: '/certificates',
-          icon: 'glasses',
+          icon: 'certificate-icon',
+          title: 'Certificates',
         },
         {
           route: '/blog',
-          icon: 'blog',
+          icon: 'blog-icon',
+          title: 'Blog',
         },
         {
           route: '/contact',
-          icon: 'phone-alt',
+          icon: 'contact-icon',
+          title: 'Contact',
         },
       ],
     };

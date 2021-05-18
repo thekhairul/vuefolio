cb29f905648bb86442a2ac9ac09101bf762fed2a
Khairul Anik <khairulanik@Khairuls-MacBook-Air.local>
2021-05-18 16:51:22 +0600

    fix and polish skillbar

--- components/Skillsbar.vue
+++ components/Skillsbar.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="skills" class="px-4">
     <ul>
-      <li v-for="(skill, idx) in skills" :key="idx">
+      <li v-for="(skill, idx) in skills" :key="idx" class="mb-2">
         <h6>{{ skill.name }}</h6>
-        <div class="relative h-4 bg-gray-300">
+        <div class="relative h-4">
+          <div
+            class="absolute w-full h-0.5"
+            style="top: 50%; margin-top: -1px"
+          ></div>
+          <div
+            class="absolute h-0.5 bg-blue-500"
+            :style="
+              `width: ${getSkillbarWidth(
+                skill.phase
+              )}%; top: 50%; margin-top: -1px`
+            "
+          ></div>
           <span
             v-for="(phase, id) in skillPhases"
             :key="id"
             :style="getSkillPhaseStyle(phase, skill.phase)"
-            class="absolute rounded-full inline-block"
+            class="rounded-full inline-block"
             :data-tooltip="phase"
           ></span>
         </div>
@@ -57,19 +69,28 @@ export default {
       const currentPhaseIndex = this.skillPhases.findIndex(
         skill => skill === currentPhase
       );
-      const color = phaseIndex <= currentPhaseIndex ? 'blue' : 'black';
-      const width = phaseIndex === currentPhaseIndex ? '12px' : '6px';
+      const color =
+        phaseIndex <= currentPhaseIndex
+          ? 'rgba(59, 130, 246, 1)'
+          : 'rgba(107, 114, 128, 1)';
+      const width = phaseIndex === currentPhaseIndex ? '12px' : '0px';
       const height = width;
       const left = (100 / this.skillPhases.length) * (phaseIndex + 1);
-
       return {
+        position: 'absolute',
         width,
         height,
         backgroundColor: color,
         top: '50%',
         left: `${left}%`,
-        transform: 'translate(-50%, -50%)',
+        transform: `translate(-${width}, -50%)`,
       };
+    },
+    getSkillbarWidth(currentPhase) {
+      const currentPhaseIndex = this.skillPhases.findIndex(
+        skill => skill === currentPhase
+      );
+      return (100 / this.skillPhases.length) * (currentPhaseIndex + 1);
     },
   },
 };

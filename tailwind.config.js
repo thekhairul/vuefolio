19f5da951c5c4ff034b24dd37a4c37eb3fa9e964
thekhairul <thekhairul.office@gmail.com>
2021-10-12 20:40:39 +0600

    add data folder to tailwind purge list

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
+    './data/**/*.{js,ts}',
     './nuxt.config.{js,ts}'
   ],
   darkMode: 'class', // or 'media' or 'class'
